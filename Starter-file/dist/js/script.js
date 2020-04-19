158e1aab6b1528a5245dfa2c6e5685d76927e2ca
Gur <guermoud.@gmail.com>
2020-04-19 18:03:49 +0000

    fixing problems

--- Starter-file/dist/js/script.js
+++ Starter-file/dist/js/script.js
@@ -1,3 +1,4 @@
+// peambule
 let preambule = document.querySelector(".preambule");
 let test1 = document.querySelector(".test1");
 let button = document.querySelector(".preambule__link");
@@ -5,5 +6,25 @@ let button = document.querySelector(".preambule__link");
 button.addEventListener("click", function() {
     preambule.style.display = "none";
     test1.style.display = "block";
+
+});
+
+// test2
+
+let precedante = document.querySelector("#pre");
+let suivante = document.querySelector("#suiv");
+let test2 = document.querySelector(".test2");
+let test3 = document.querySelector(".test3");
+
+suivante.addEventListener("click", function()
+{
+test3.style.display = "block";
+test2.style.display = "none";
+
+});
+precedante.addEventListener("click", function()
+{
+test1.style.display = "block";
+test2.style.display = "none";
 
 });
