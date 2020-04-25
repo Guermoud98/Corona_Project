18011ed99bd4457240f86a8c8716a9362b24b04d
Gur <guermoud.@gmail.com>
2020-04-25 15:07:51 +0000

    .

--- Starter-file/dist/js/script.js
+++ Starter-file/dist/js/script.js
@@ -1,30 +1,32 @@
-// peambule
-let preambule = document.querySelector(".preambule");
-let test1 = document.querySelector(".test1");
-let button = document.querySelector(".preambule__link");
+// // peambule
+// let preambule = document.querySelector(".preambule");
+// let test1 = document.querySelector(".test1");
+// let button = document.querySelector(".preambule__link");
 
-button.addEventListener("click", function() {
-    preambule.style.display = "none";
-    test1.style.display = "block";
+// button.addEventListener("click", function() {
+//     preambule.style.display = "none";
+//     test1.style.display = "block";
 
-});
+// });
+
+// test3 
 
 // test2
 
-let precedante = document.querySelector("#pre");
-let suivante = document.querySelector("#suiv");
-let test2 = document.querySelector(".test2");
-let test3 = document.querySelector(".test3");
+// let precedante = document.querySelector("#pre");
+// let suivante = document.querySelector("#suiv");
+// let test2 = document.querySelector(".test2");
+// let test3 = document.querySelector(".test3");
 
-suivante.addEventListener("click", function()
-{
-test3.style.display = "block";
-test2.style.display = "none";
+// suivante.addEventListener("click", function()
+// {
+// test3.style.display = "block";
+// test2.style.display = "none";
 
-});
-precedante.addEventListener("click", function()
-{
-test1.style.display = "block";
-test2.style.display = "none";
+// });
+// precedante.addEventListener("click", function()
+// {
+// test1.style.display = "block";
+// test2.style.display = "none";
 
-});
+// });
